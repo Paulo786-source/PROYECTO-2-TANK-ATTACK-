72d5c01e03b563c2b1ddbe258ec2b2679f3d3ab2
Paulo786-source <centenopaulo786@gmail.com>
2026-05-17 20:49:56 -0600

    Clase bullet

--- TankAttack/Bullet.h
+++ TankAttack/Bullet.h
@@ -1,5 +1,23 @@
 #pragma once
+#include "Pathfinding.h"
+#include "Tank.h"
+
+
 class Bullet
 {
+private:
+	
+	BulletPath trail;
+	Tank* shooter;
+	bool fullPower;
+
+public:
+
+	Bullet(BulletPath trail, Tank* shooter, bool fullPower);
+
+	const BulletPath& getBulletPath() const;
+	Tank* getShooter() const;
+	bool isFullPower() const;
+
 };
 
